f07c0b709a112b77a431fa1b2fe2988780473132
Justananonomous <115206691+Justananonomous@users.noreply.github.com>
2023-06-19 02:00:28 -0600

    Update glyphLogic.ts

--- src/glyphLogic.ts
+++ src/glyphLogic.ts
@@ -87,11 +87,11 @@ export function checkGlyphs(inputElement: HTMLInputElement, enableLengthCheck: b
 	const regions = getRegions(galaxy);
 
 	const correctLength = glyphs.length == expectedGlyphLength;
-	const regionInHub = regions.has(regionGlyphs);
+	const regionInCore = regions.has(regionGlyphs);
 	const reachable = (decSIV && decSIV < (maxIndex + 1)) as boolean;
 	const isValid = (() => {
 		if (enableLengthCheck || glyphs.length == expectedGlyphLength) {
-			return correctLength && regionInHub && reachable;
+			return correctLength && regionInCore && reachable;
 		}
 
 		if (glyphs.length != expectedGlyphLength) {
@@ -108,8 +108,8 @@ export function checkGlyphs(inputElement: HTMLInputElement, enableLengthCheck: b
 			return { isValid: false, error: 'Invalid glyph sequence: Incorrect length' };
 		}
 
-		if (!regionInHub) {
-			return { isValid: false, error: 'Invalid glyph sequence: No Hub region' };
+		if (!regionInCore) {
+			return { isValid: false, error: 'Invalid glyph sequence: No Core region' };
 		}
 
 		if (!reachable) {
